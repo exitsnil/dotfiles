c507481bd67cba9fcc6536167a606dc1ae77491f
Adam Kessler <exitsnil@gmail.com>
2025-10-22 21:21:14 -0400

    add original keybindings

--- nvim/.config/nvim/lib/lsp.lua
+++ nvim/.config/nvim/lib/lsp.lua
@@ -3,4 +3,6 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
