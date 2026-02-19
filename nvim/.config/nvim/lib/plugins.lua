8153463696f830aef55558193eb37aac0fc1cbca
Adam Kessler <exitsnil@icloud.com>
2026-02-19 17:07:50 -0500

    JA

--- nvim/.config/nvim/lib/plugins.lua
+++ nvim/.config/nvim/lib/plugins.lua
@@ -50,7 +50,9 @@ lazy.setup({
     require("plugins.zig-vim"),
     require("plugins.gitsigns-nvim"),
     require("plugins.vim-dadbod"),
+    require("plugins.tokyonight"),
     require("plugins.cursor-dark"),
-    require("plugins.vscode-nvim")
+    require("plugins.vscode-nvim"),
+    require("plugins.onedark-nvim")
 	},
 })
