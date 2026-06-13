d6427e601be691ebf4b9171d70546c58d1612dc1
Adam Kessler <exitsnil@icloud.com>
2026-06-12 20:06:03 -0400

    update plugins add parsers

--- nvim/.config/nvim/lib/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lib/plugins/nvim-treesitter.lua
@@ -1,55 +1,3 @@
--- TSInstall asm
--- TSInstall awk
--- TSInstall bash
--- TSInstall c
--- TSInstall cmake
--- TSInstall commonlisp
--- TSInstall cpp
--- TSInstall csv
--- TSInstall cuda
--- TSInstall elixir
--- TSinstall git_config
--- TSInstall git_rebase
--- TSInstall gitattributes
--- TSInstall gitcommit
--- TSInstall gitignore
--- TSInstall glsl
--- TSInstall go
--- TSInstall gomod
--- TSInstall gowork
--- TSInstall gpg
--- TSInstall graphql
--- TSInstall html
--- TSInstall http
--- TSInstall java
--- TSinstall javascript
--- TSInstall jq
--- TSInstall jsdoc
--- TSInstall json
--- TSInstall llvm
--- TSInstall lua
--- TSInstall luadoc
--- TSInstall m68k
--- TSInstall make
--- TSinstall markdown
--- TSInstall meson
--- TSInstall nasm
--- TSInstall ninja
--- TSInstall objc
--- TSInstall ocaml
--- TSInstall python
--- TSInstall ruby
--- TSInstall rust
--- TSInstall scala
--- TSInstall scheme
--- TSInstall sql
--- TSInstall ssh_config
--- TSInstall strace
--- TSInstall tsx
--- TSInstall typescript
--- TSInstall vim
--- TSInstall yaml
-
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
@@ -59,12 +7,19 @@ return {
 			"asm",
 			"awk",
 			"bash",
+			"bpftrace",
 			"c",
+			"clojure",
 			"cmake",
 			"commonlisp",
 			"cpp",
 			"csv",
 			"cuda",
+			"desktop",
+			"devicetree",
+			"diff",
+			"dockerfile",
+			"eex",
 			"elixir",
 			"git_config",
 			"git_rebase",
@@ -79,10 +34,15 @@ return {
 			"graphql",
 			"html",
 			"http",
+			"java",
+			"javadoc",
 			"javascript",
 			"jq",
 			"jsdoc",
 			"json",
+			"kotlin",
+			"kconfig",
+			"linkerscript",
 			"llvm",
 			"lua",
 			"luadoc",
@@ -91,9 +51,12 @@ return {
 			"markdown",
 			"meson",
 			"nasm",
+			"nginx",
 			"ninja",
 			"objc",
 			"ocaml",
+			"perl",
+			"proto",
 			"python",
 			"ruby",
 			"rust",
@@ -102,9 +65,13 @@ return {
 			"sql",
 			"ssh_config",
 			"strace",
+			"teal",
+			"tmux",
+			"toml",
 			"tsx",
 			"typescript",
 			"vim",
+			"xml",
 			"yaml",
 		})
 
@@ -114,65 +81,4 @@ return {
 			end,
 		})
 	end,
-	-- opts = {
-	-- 	ensure_installed = {
-	-- 		"asm",
-	-- 		"awk",
-	-- 		"bash",
-	-- 		"c",
-	-- 		"cmake",
-	-- 		"commonlisp",
-	-- 		"cpp",
-	-- 		"csv",
-	-- 		"cuda",
-	-- 		"elixir",
-	-- 		"git_config",
-	-- 		"git_rebase",
-	-- 		"gitattributes",
-	-- 		"gitcommit",
-	-- 		"gitignore",
-	-- 		"glsl",
-	-- 		"go",
-	-- 		"gomod",
-	-- 		"gowork",
-	-- 		"gpg",
-	-- 		"graphql",
-	-- 		"html",
-	-- 		"http",
-	-- 		"javascript",
-	-- 		"jq",
-	-- 		"jsdoc",
-	-- 		"json",
-	-- 		"llvm",
-	-- 		"lua",
-	-- 		"luadoc",
-	-- 		"m68k",
-	-- 		"make",
-	-- 		"markdown",
-	-- 		"meson",
-	-- 		"nasm",
-	-- 		"ninja",
-	-- 		"objc",
-	-- 		"ocaml",
-	-- 		"python",
-	-- 		"ruby",
-	-- 		"rust",
-	-- "scala",
-	-- 		"scheme",
-	-- 		"sql",
-	-- 		"ssh_config",
-	-- 		"strace",
-	-- 		"tsx",
-	-- 		"typescript",
-	-- 		"vim",
-	-- 		"yaml",
-	-- 	},
-	-- 	sync_install = false,
-	-- 	highlight = {
-	-- 		enable = true,
-	-- 	},
-	-- 	indent = {
-	-- 		enable = true,
-	-- 	},
-	-- },
 }
