228d66786304bc4c6f6fb62c9c5127bac4014088
Adam Kessler <exitsnil@icloud.com>
2026-06-24 12:33:38 -0400

    make LSP hover nicer

--- nvim/.config/nvim/lib/lsp.lua
+++ nvim/.config/nvim/lib/lsp.lua
@@ -4,5 +4,19 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
+vim.keymap.set("n", "K", function()
+	vim.lsp.buf.hover({
+		border = "rounded",
+		max_width = 80,
+		max_height = 24,
+	})
+end, { desc = "LSP Hover" })
+
+vim.keymap.set("n", "<leader>sh", function()
+	vim.lsp.buf.signature_help({
+		border = "rounded",
+		max_width = 80,
+		max_height = 25,
+	})
+end, { desc = "Signature Help" })
